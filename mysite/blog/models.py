de0d36669e29cc302a5d1783746a3d64c68a65a4
lukelavine <sacul.lml@gmail.com>
2022-12-05 16:22:55 -0800

    Added sort and search to dev blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+	id = models.IntegerField(primary_key=True)
 	published = models.DateTimeField('date published')
 	title = models.CharField(max_length = 100)
 	text = models.TextField(blank=True)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from blog.models import Post
 
+#this is bad practice and should probably be handled by js instead
+#it works, but when a user refreshes it asks about form resubmission
 def index(request):
-	posts = Post.objects.all().order_by('-published')
+	selected = ""
+	if request.method == "POST":
+		if request.POST.get('search') != None:
+			posts = Post.objects.filter(text__icontains=request.POST.get('search'))
+		elif request.POST.get('sort') == 'date_a':
+			posts = Post.objects.all().order_by('published')
+			selected = "date_a"
+		elif request.POST.get('sort') == 'alpha_d':
+			posts = Post.objects.all().order_by('title')
+			selected = "alpha_d"
+		elif request.POST.get('sort') == 'alpha_a':
+			posts = Post.objects.all().order_by('-title')
+			selected = "alpha_a"
+		else:
+			posts = Post.objects.all().order_by('-published')
+	else:
+		posts = Post.objects.all().order_by('-published')
 	context = {
 		"posts": posts,
+		"selected": selected
 	}
 	return render(request, "blog_index.html", context)
 
