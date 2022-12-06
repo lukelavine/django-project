3c13a589e9efab21e5b81a90cbc74b9bc98cdac7
lukelavine <sacul.lml@gmail.com>
2022-12-05 16:24:48 -0800

    Removed sort and search for blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,26 +4,9 @@ from blog.models import Post
 #this is bad practice and should probably be handled by js instead
 #it works, but when a user refreshes it asks about form resubmission
 def index(request):
-	selected = ""
-	if request.method == "POST":
-		if request.POST.get('search') != None:
-			posts = Post.objects.filter(text__icontains=request.POST.get('search'))
-		elif request.POST.get('sort') == 'date_a':
-			posts = Post.objects.all().order_by('published')
-			selected = "date_a"
-		elif request.POST.get('sort') == 'alpha_d':
-			posts = Post.objects.all().order_by('title')
-			selected = "alpha_d"
-		elif request.POST.get('sort') == 'alpha_a':
-			posts = Post.objects.all().order_by('-title')
-			selected = "alpha_a"
-		else:
-			posts = Post.objects.all().order_by('-published')
-	else:
-		posts = Post.objects.all().order_by('-published')
+	posts = Post.objects.all().order_by('-published')
 	context = {
-		"posts": posts,
-		"selected": selected
+		"posts": posts
 	}
 	return render(request, "blog_index.html", context)
 
