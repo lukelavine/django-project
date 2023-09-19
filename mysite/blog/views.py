5e0193f830d8a420ea5156f31beb6c8ae919014a
lukelavine <sacul.lml@gmail.com>
2023-09-19 10:58:39 -0700

    displays one devlog post at a time, navigate back and forth between them footer always at bottom of page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
 from blog.models import Post
 
-#this is bad practice and should probably be handled by js instead
-#it works, but when a user refreshes it asks about form resubmission
+#give the latest
 def index(request):
-	posts = Post.objects.all().order_by('-published')
+	post = Post.objects.latest('published')
 	context = {
-		"posts": posts
+		"post": post,
+		"prev": post.id-1,
+		"next": 0
 	}
-	return render(request, "blog_index.html", context)
+	return render(request, "blog_detail.html", context)
 
 def detail(request, pk):
 	post = Post.objects.get(pk=pk)
+	latest = Post.objects.latest('published')
+	next = pk + 1
+	if post.id == latest.id:
+		next = 0
 	context = {
-		"post": post
+		"post": post,
+		"next": next,
+		"prev": pk-1
 	}
 	return render(request, "blog_detail.html", context)
+
+#TODO display all devlog post titles on one page
+def all(request):
+	return None
