e2a4c875a85f469650fed55c3ab33806643247bc
lukelavine <sacul.lml@gmail.com>
2023-11-14 14:48:38 -0800

    Revamped home page, added portfolio projects and rewording some sections

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,9 +4,14 @@ from django.shortcuts import render
 from django.utils import timezone
 
 from blog.models import Post
+from portfolio.models import PortfolioEntry
 
 # Create your views here.
 def home(request):
 	recent_post = Post.objects.latest('published')
-	context = {'recent_post': recent_post}
+	project1 = PortfolioEntry.objects.get(pk=3)
+	project2 = PortfolioEntry.objects.get(pk=2)
+	context = {'recent_post': recent_post,
+				'project1': project1,
+				'project2': project2}
 	return render(request, 'home/index.html', context)
